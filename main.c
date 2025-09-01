c12ba6dec4c53871cc0fdb6f78fbc8fadb5546aa
malakaiW <malakaiwinters2006@gmail.com>
2025-08-31 22:07:48 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+int num1 = 1;
+int num2 = 10;
+int num3 = -100;
+
+printf("%+d\n", num1);
+printf("%+d\n", num2);
+printf("%+d\n", num3);    
+return 0;
+
+
+
+}
