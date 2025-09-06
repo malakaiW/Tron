8bde75eeafa4438b3559c93014416477f34d2ebe
malakaiW <malakaiwinters2006@gmail.com>
2025-09-05 22:16:59 -0700

    calculating interest gains

---
+++ sigma.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double principle = 0.0;
+    double rate = 0.0;
+    int years = 0;
+    double timescomp = 0.0;
+    double total = 0.0;
+
+    printf("compound interest calculator\n");
+
+    printf("enter the principal (p): ");
+    scanf("%lf", &principle);
+
+    printf("Enter the interest rate %(r): ");
+    scanf("%lf", &rate);
+    rate = rate / 100;
+
+    printf("enter the number of years : ");
+    scanf("%d", &years);
+
+    printf("enter number of times compounded p years (n): ");
+    scanf("%lf", &timescomp);
+
+    total = principle*(pow((1+(rate/timescomp)),timescomp * years));
+    printf("%lf", total);
+
+
+
+
+
+}
